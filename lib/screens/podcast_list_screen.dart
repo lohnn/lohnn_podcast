08ffb23664d4c73063342a0c0602ba305664d01a
lohnn <johannes@lohnn.se>
2024-05-02 22:27:49 +0200

    WIP: Hacky first implementation of using FirestoreListView

--- lib/screens/podcast_list_screen.dart
+++ lib/screens/podcast_list_screen.dart
@@ -1,24 +1,29 @@
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:podcast/data/podcast.dart';
+import 'package:podcast/providers/firebase/firestore/firestore_provider.dart';
 import 'package:podcast/providers/firebase/firestore/podcast_list_pod_provider.dart';
-import 'package:podcast/screens/async_value_screen.dart';
 import 'package:podcast/screens/dialogs/add_podcast_dialog.dart';
 import 'package:podcast/screens/loading_screen.dart';
 import 'package:podcast/widgets/podcast_list_tile.dart';
 
-class PodcastListScreen extends AsyncValueWidget<List<PodcastListRow>> {
+class PodcastListScreen extends ConsumerWidget {
   const PodcastListScreen({super.key});
 
   @override
-  ProviderBase<AsyncValue<List<PodcastListRow>>> get provider =>
-      podcastListPodProvider;
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: Move back to provider
+    final firestore = ref.watch(firestoreProvider.notifier);
+    final data = firestore.userCollection('podcastList').withConverter(
+      fromFirestore: (snapshot, __) {
+        return Podcast.fromJson(snapshot.data()!);
+      },
+      toFirestore: (podcast, __) {
+        return podcast.toJson();
+      },
+    );
 
-  @override
-  Widget buildWithData(
-    BuildContext context,
-    WidgetRef ref,
-    AsyncData<List<PodcastListRow>> data,
-  ) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -39,21 +44,11 @@ class PodcastListScreen extends AsyncValueWidget<List<PodcastListRow>> {
       ),
       body: RefreshIndicator(
         onRefresh: ref.read(podcastListPodProvider.notifier).refreshAll,
-        child: ListView.builder(
-          itemCount: data.value.length,
-          itemBuilder: (context, index) {
-            final PodcastListRow(
-              :podcast,
-              :hasNew,
-              :totalEpisodes,
-              :unlistenedEpisodes,
-            ) = data.value[index];
-
+        child: FirestoreListView<Podcast>(
+          query: data,
+          itemBuilder: (context, snapshot) {
             return PodcastListTile(
-              podcast,
-              hasNew: hasNew,
-              total: totalEpisodes,
-              unlistened: unlistenedEpisodes,
+              snapshot.data(),
             );
           },
         ),
