d06f4b9dbfd391396fddfcbc3743e8d36188621d
lohnn <johannes@lohnn.se>
2024-05-02 22:27:49 +0200

    WIP: Hacky first implementation of using FirestoreListView

--- lib/widgets/podcast_list_tile.dart
+++ lib/widgets/podcast_list_tile.dart
@@ -1,35 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:podcast/data/podcast.dart';
+import 'package:podcast/providers/firebase/firestore/podcast_list_pod_provider.dart';
 import 'package:podcast/screens/podcast_details_screen.dart';
 import 'package:podcast/widgets/rounded_image.dart';
 
 class PodcastListTile extends StatelessWidget {
   final Podcast podcast;
-  final int total;
-  final int unlistened;
-  final bool hasNew;
+
+  // final int total;
+  // final int unlistened;
+  // final bool hasNew;
 
   const PodcastListTile(
     this.podcast, {
     super.key,
-    required this.hasNew,
-    required this.total,
-    required this.unlistened,
+    // required this.hasNew,
+    // required this.total,
+    // required this.unlistened,
   });
 
   @override
   Widget build(BuildContext context) {
+    // final PodcastListRow(
+    //   :podcast,
+    //   :hasNew,
+    //   totalEpisodes: total,
+    //   unlistenedEpisodes: unlistened,
+    // ) = podcastRow;
     return ListTile(
       onTap: () {
         // @TODO: Navigate named
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => PodcastDetailsScreen(podcast)),
+          MaterialPageRoute(
+            builder: (context) => PodcastDetailsScreen(podcast),
+          ),
         );
       },
-      trailing: Text('$unlistened/$total'),
+      // trailing: Text('$unlistened/$total'),
       leading: RoundedImage(
         imageUrl: podcast.image,
-        showDot: hasNew,
+        showDot: true,
       ),
       title: Text(podcast.name),
     );
