cd1d2631fe9184ff348161e128238d00232ad86d
lohnn <johannes@lohnn.se>
2024-05-13 16:02:16 +0200

    More color scheme updates to make app more pretty

--- lib/screens/logged_in_screen.dart
+++ lib/screens/logged_in_screen.dart
@@ -55,7 +55,7 @@ class LoggedInScreen extends HookConsumerWidget {
         .valueOrNull;
 
     return AnimatedTheme(
-      key: const ValueKey('Top level theme'),
+      key: const ValueKey('LoggedInScreen.theme'),
       data: Theme.of(context).copyWith(colorScheme: colorScheme),
       child: PodcastActions(
         child: Shortcuts(
