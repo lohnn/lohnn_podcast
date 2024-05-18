fb5ad5106e12c5e0017dfe0dab0116d65b407e87
lohnn <johannes@lohnn.se>
2024-05-18 15:39:25 +0200

    Refactor episode lists in podcasts

--- lib/extensions/episode_snapshot_extension.dart
+++ lib/extensions/episode_snapshot_extension.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:podcast/data/episode.dart';
 import 'package:podcast/data/podcast.dart';
+import 'package:podcast/extensions/map_extensions.dart';
 
 extension EpisodeSnapshotExtension on DocumentSnapshot<Episode> {
   Future<void> markUnlistened() async {
@@ -13,9 +14,9 @@ extension EpisodeSnapshotExtension on DocumentSnapshot<Episode> {
     final podcast = (await podcastReference.get()).data()!;
     await podcastReference.set(
       podcast.copyWith(
-        listenedEpisodesList: {
-          for (final episode in podcast.listenedEpisodesList)
-            if (episode.id != reference.id) episode,
+        listenedEpisodes: {
+          for (final (episode, episodeHash) in podcast.listenedEpisodes.records)
+            if (episode.id != reference.id) episode: episodeHash,
         },
       ),
     );
@@ -24,21 +25,22 @@ extension EpisodeSnapshotExtension on DocumentSnapshot<Episode> {
   Future<void> markListened() async {
     if (data() case final currentEpisode?) {
       await reference.set(currentEpisode.copyWith(listened: true));
-    }
 
-    // Add episode reference to the podcast's "listenedList"
-    final podcastReference = reference.podcast;
-    final podcast = (await podcastReference.get()).data()!;
-    await podcastReference.set(
-      podcast.copyWith(
-        listenedEpisodesList: {
-          // Just make damn sure we don't add an episode more than once
-          for (final episode in podcast.listenedEpisodesList)
-            if (episode.id != reference.id) episode,
-          reference,
-        },
-      ),
-    );
+      // Add episode reference to the podcast's "listenedList"
+      final podcastReference = reference.podcast;
+      final podcast = (await podcastReference.get()).data()!;
+      await podcastReference.set(
+        podcast.copyWith(
+          listenedEpisodes: {
+            // Just make damn sure we don't add an episode more than once
+            for (final (episode, episodeHash)
+                in podcast.listenedEpisodes.records)
+              if (episode.id != reference.id) episode: episodeHash,
+            reference: EpisodeHash.fromEpisode(currentEpisode),
+          },
+        ),
+      );
+    }
   }
 }
 
