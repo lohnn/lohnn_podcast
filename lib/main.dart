aedae3bb3f301fb867ed05072fbb22dd77c01e6b
lohnn <johannes@lohnn.se>
2024-06-07 15:25:11 +0200

    Darken background during intro animation on dark mode devices

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:podcast/extensions/nullability_extensions.dart';
 import 'package:podcast/firebase_options.dart';
 import 'package:podcast/hooks/use_rive_controller.dart';
 import 'package:podcast/providers/firebase/user_provider.dart';
@@ -71,6 +72,14 @@ class EntryAnimationScreen extends HookWidget {
                 Positioned.fill(
                   child: createCompatVectorGraphic(
                     fit: BoxFit.cover,
+                    colorFilter:
+                        (Theme.of(context).brightness == Brightness.dark)
+                            .thenOrNull(
+                      () => ColorFilter.mode(
+                        Colors.black.withOpacity(0.4),
+                        BlendMode.darken,
+                      ),
+                    ),
                     loader: const AssetBytesLoader(
                       'assets/vectors/background.svg',
                     ),
