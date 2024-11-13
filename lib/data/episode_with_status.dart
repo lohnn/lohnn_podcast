9064ea6b52f1b7d8a5eabb70b94f4e77d3fa5dbc
lohnn <johannes@lohnn.se>
2024-11-13 12:30:14 +0100

    Syncing current position seems to be working

--- lib/data/episode_with_status.dart
+++ lib/data/episode_with_status.dart
@@ -1,7 +1,6 @@
 import 'package:dart_mappable/dart_mappable.dart';
 import 'package:podcast/data/episode.model.dart';
 import 'package:podcast/data/user_episode_status.model.dart';
-import 'package:podcast/services/podcast_audio_handler.dart';
 
 part 'episode_with_status.mapper.dart';
 
@@ -14,15 +13,4 @@ class EpisodeWithStatus with EpisodeWithStatusMappable {
     required this.episode,
     this.status,
   });
-
-  PodcastMediaItem mediaItem({
-    Duration? actualDuration,
-  }) =>
-      PodcastMediaItem(
-        episode: this,
-        id: episode.url.uri.toString(),
-        title: episode.title,
-        artUri: episode.imageUrl.uri,
-        duration: actualDuration ?? episode.duration?.duration,
-      );
 }
