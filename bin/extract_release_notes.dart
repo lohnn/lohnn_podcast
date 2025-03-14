754e626ee67915aaea735a41365b4cc6a95b3c5d
lohnn <johannes@lohnn.se>
2025-03-14 13:49:16 +0100

    Release notes are now stored in file to be more stable.

--- bin/extract_release_notes.dart
+++ bin/extract_release_notes.dart
@@ -3,6 +3,14 @@ import 'dart:io';
 import 'package:pubspec_parse/pubspec_parse.dart';
 
 void main(List<String> args) {
+  final String? outputFile;
+  if (args.indexOf('-o') case final fileIndex when fileIndex != -1 &&
+      args.length > fileIndex + 1) {
+    outputFile = args[fileIndex + 1];
+  } else {
+    outputFile = null;
+  }
+
   final pubspecFile = File('apps/podcast/pubspec.yaml');
   final pubspecContent = pubspecFile.readAsStringSync();
   final pubspec = Pubspec.parse(pubspecContent);
@@ -32,6 +40,13 @@ void main(List<String> args) {
     null || '' => 'No release notes found for version $releaseVersion',
     final notes => notes,
   };
+
+  if (outputFile != null) {
+    File(outputFile)
+      ..createSync(recursive: true)
+      ..writeAsStringSync(changelogForVersion);
+    stdout.writeln('Release notes written to $outputFile');
+  }
 
   stdout.writeln(changelogForVersion);
 }
