907d9aa53c1849a4976054efa1c78919f21f8876
lohnn <johannes@lohnn.se>
2025-06-02 16:53:46 +0200

    Move color scheme override into PodcastApp

--- packages/podcast_core/lib/widgets/podcast_app.dart
+++ packages/podcast_core/lib/widgets/podcast_app.dart
@@ -1,36 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:podcast_core/providers/episode_color_scheme_provider.dart';
 
-class PodcastApp extends HookWidget {
+class PodcastApp extends HookConsumerWidget {
   final Widget child;
 
   const PodcastApp({super.key, required this.child});
 
-  Future<({ColorScheme light, ColorScheme dark})> _loadColorScheme() async {
+  Future<ColorScheme> _loadDefaultColorScheme(Brightness brightness) async {
     const icon = AssetImage(
       'assets/icons/app_icon.webp',
       package: 'podcast_core',
     );
-    final lightColorScheme = await ColorScheme.fromImageProvider(
+    return await ColorScheme.fromImageProvider(
       provider: icon,
+      brightness: brightness,
     );
-    final darkColorScheme = await ColorScheme.fromImageProvider(
-      provider: icon,
-      brightness: Brightness.dark,
-    );
-
-    return (light: lightColorScheme, dark: darkColorScheme);
   }
 
   @override
-  Widget build(BuildContext context) {
-    final iconColorSchemeFuture = useMemoized(_loadColorScheme);
-    final iconColorScheme = useFuture(iconColorSchemeFuture);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final brightness = MediaQuery.platformBrightnessOf(context);
+    final iconColorSchemeFuture = useMemoized(() {
+      return _loadDefaultColorScheme(brightness);
+    }, [brightness]);
+    final iconColorScheme = useFuture(iconColorSchemeFuture).data;
 
-    if (iconColorScheme.data case (:final light, :final dark)) {
+    final colorScheme = ref
+        .watch(currentPlayingEpisodeColorSchemeProvider(brightness))
+        .value;
+
+    if (colorScheme ?? iconColorScheme case final colorScheme?) {
       return MaterialApp(
-        theme: ThemeData.light().copyWith(colorScheme: light),
-        darkTheme: ThemeData.dark().copyWith(colorScheme: dark),
+        theme: ThemeData.light().copyWith(colorScheme: colorScheme),
+        darkTheme: ThemeData.dark().copyWith(colorScheme: colorScheme),
         debugShowCheckedModeBanner: false,
         home: child,
       );
